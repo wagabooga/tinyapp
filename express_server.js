f75ee9ffc3e74b1dc799d80255f7615b150ba4bc
wagabooga <mattanthu@hotmail.com>
2021-09-28 22:13:02 +0000

    added bodyParser

--- express_server.js
+++ express_server.js
@@ -1,12 +1,24 @@
-// require
-const express = require("express");
 
+
+
+
+// express
+const express = require("express");
 const app = express();// using variable "app" for express
+// bodyParser
+const bodyParser = require("body-parser");
+app.use(express.urlencoded({extended: true}))
+
+// server
 const PORT = 8080; // default port 8080
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
 // template
 app.set("view engine", "ejs");
 
+//database
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -17,7 +29,11 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-
+// above urls/:id's
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+//
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
@@ -33,6 +49,6 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+
+
+
