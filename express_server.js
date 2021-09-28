564fdb6f4242bcce8d91ac5861ed2dbed40d2f18
wagabooga <mattanthu@hotmail.com>
2021-09-28 21:02:11 +0000

    added /urls

--- express_server.js
+++ express_server.js
@@ -12,9 +12,16 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
