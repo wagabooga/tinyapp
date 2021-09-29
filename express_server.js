44fb829c0c9fa7f08fc0759a98e50ff8d8f30597
wagabooga <mattanthu@hotmail.com>
2021-09-29 07:03:00 +0000

    added delete button and makes redirect back to /urls after deleting

--- express_server.js
+++ express_server.js
@@ -60,6 +60,12 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL]
+  // refresh
+  res.redirect(`/urls`)
+});
+
 // redirect link
 app.get("/u/:shortURL", (req, res) => {
   if (urlDatabase[req.params.shortURL]){
@@ -69,6 +75,7 @@ app.get("/u/:shortURL", (req, res) => {
     res.render("u_not_found");
   }
 });
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
