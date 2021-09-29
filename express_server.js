4a2967bcb64f8798f1cf83b0c444c66d144cbabd
wagabooga <mattanthu@hotmail.com>
2021-09-29 06:12:23 +0000

    added user redirect from shortURL link to longURL link

--- express_server.js
+++ express_server.js
@@ -60,6 +60,12 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL
+  let longURL = urlDatabase[shortURL] 
+  res.redirect(`${longURL}`); 
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
