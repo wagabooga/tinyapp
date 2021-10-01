0cebd35b31025ba9a5ad3da2b97e55f2f81dc111
wagabooga <mattanthu@hotmail.com>
2021-10-01 00:23:40 +0000

    redirect to login if no cookie is found on url/new

--- express_server.js
+++ express_server.js
@@ -91,6 +91,9 @@ app.get("/urls", (req, res) => {
 
 // above urls/:id's
 app.get("/urls/new", (req, res) => {
+  if (!req.cookies["user_id"]){
+    res.redirect(`/login`)
+  }
   const templateVars = {user: users[req.cookies["user_id"]]}
   res.render("urls_new", templateVars);
 });
